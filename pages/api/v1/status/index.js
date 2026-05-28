e0bd6180557540f32caa0b0b30bfd8c89c4b8cea
Caique.Dev <105514882+Caique-S@users.noreply.github.com>
2026-05-28 04:34:54 +0000

    adicionado o  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-  res.status(200).json({ mensagem: "Missão realizada com Êxito - API" });
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1+1 as sum");
+  console.log(result);
+  res.status(200).json({ mensagem: "Missão foi realizada com Êxito - API" });
 }
 
 export default status;
